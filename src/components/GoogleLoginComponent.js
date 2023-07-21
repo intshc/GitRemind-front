a840444e6249a3e49b3243b49c9ed03886506a2c
Hyeongchan Shin <intshc@gmail.com>
2023-07-21 17:28:09 +0900

    구글 로그인 성공시 header에 jwt값 파싱해서 json으로 정보 가져옴

--- src/components/GoogleLoginComponent.js
+++ src/components/GoogleLoginComponent.js
@@ -1,24 +1,42 @@
-import {Button, Typography} from "@mui/material";
-import googleIcon from "../images/googleIcon.png"
+import {GoogleLogin} from "@react-oauth/google";
 
-const GoogleLoginComponent = ({ userName }) => {
+const apikey = process.env.REACT_APP_googleClientId;
+
+
+const GoogleLoginComponent = () => {
+
+  const onFailure = (response) => {
+    console.log('Login failed response:', response);
+  };
+  function parseJwt (token) {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+  }
+
+  function handleCredentialResponse(response) {
+    //console.log(response)
+
+    console.log(JSON.stringify(parseJwt(response.credential)));
+  }
   return (
-          <div className="col-md-6">
+          <div>
+            <GoogleLogin
+                    clientId={apikey}
+                    buttonText="Login with Google"
+                    onSuccess={handleCredentialResponse}
+                    onFailure={onFailure}
+                    cookiePolicy={'single_host_origin'}
+            />
 
-            {userName ? (
-                    <>
-                      Logged in as: <Typography component="span" id="user">{userName}</Typography>
-                      <Button href="/logout" variant="contained"
-                      >Logout</Button>
-                    </>
-            ) : (
-                    <a href="/oauth2/authorization/google">
-                      <img src={googleIcon} alt={"구글로 로그인 하기"}/>
-                    </a>
-
-            )}
           </div>
   );
 };
+
+
 
 export default GoogleLoginComponent;
